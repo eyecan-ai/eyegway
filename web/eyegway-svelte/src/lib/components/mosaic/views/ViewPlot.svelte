bf6c782c4136620320398d4f77fdc847096eb4f8
Marco Toschi <marco.toschi@fastwebnet.it>
2024-10-30 19:23:42 +0100

    fix: :bug: fix reactivity to events during reload

--- web/eyegway-svelte/src/lib/components/mosaic/views/ViewPlot.svelte
+++ web/eyegway-svelte/src/lib/components/mosaic/views/ViewPlot.svelte
@@ -5,19 +5,6 @@
 
 	export let userData: DataPlot | null = null;
 
-	let relayouting: boolean = false;
-	let relayout: boolean = false;
-
-	function parseObject<T>(obj: any, template: T): T {
-		const result: Partial<T> = {};
-		for (const key in template) {
-			if (obj.hasOwnProperty(key) && typeof obj[key] === typeof template[key]) {
-				result[key] = obj[key];
-			}
-		}
-		return result as T;
-	}
-
 	function unflattenObject(data: any) {
 		const result: any = {};
 		for (const key in data) {
@@ -49,30 +36,35 @@
 	let layout: Partial<Layout> = {};
 	let config: Partial<Config> = {};
 
-	$: if (userData && !relayouting) {
-		data = userData.data;
-		if (!relayout) layout = parseObject(userData.layout, layout);
-		config = parseObject(userData.config, config);
-	}
+	let relayouting: boolean = false;
+	let relayout: boolean = false;
+
 	function handleRelayouting(event: CustomEvent) {
+		layout = deepMerge(layout, unflattenObject(event.detail));
 		relayouting = true;
 	}
 	function handleRelayout(event: CustomEvent) {
-		layout = deepMerge(layout, unflattenObject(event.detail));
 		relayouting = false;
+		layout = deepMerge(layout, unflattenObject(event.detail));
 		relayout = true;
+	}
+
+	$: if (userData) {
+		data = structuredClone(userData.data);
+		if (!relayouting && !relayout) layout = structuredClone(userData.layout);
+		config = structuredClone(userData.config);
 	}
 </script>
 
-{#if userData}
-	<Plot
-		{data}
-		{layout}
-		{config}
-		fillParent={true}
-		debounce={1}
-		configReactivityStrategy={'none'}
-		on:relayouting={handleRelayouting}
-		on:relayout={handleRelayout}
-	/>
-{/if}
+<Plot
+	bind:data
+	bind:layout
+	bind:config
+	fillParent={true}
+	debounce={0}
+	configReactivityStrategy={'none'}
+	on:relayouting={handleRelayouting}
+	on:restyle={handleRelayout}
+	on:relayout={handleRelayout}
+	on:afterPlot={handleRelayout}
+/>
