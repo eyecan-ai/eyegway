8cf949e6a77b0b1ac2d4166c94077431bf601bbc
mmarcoo <marco.toschi@fastwebnet.it>
2024-12-17 17:28:56 +0100

    fix: :bug: re-brought env var system to svelte standard

--- web/eyegway-svelte/src/lib/Parameters.ts
+++ web/eyegway-svelte/src/lib/Parameters.ts
@@ -1,9 +1,11 @@
+import { env } from '$env/dynamic/public'
+
 export class Parameters {
-	static host: string = import.meta.env.EYEGWAY_HOST || 'http://localhost:55221';
-	static hubTriggerKey: string = import.meta.env.EYEGWAY_HUB_TRIGGER_KEY || '';
-	static title: string = import.meta.env.EYEGWAY_TITLE || 'eyegway';
-	static themes: string = import.meta.env.EYEGWAY_THEMES || 'ayu_light,ayu_mirage,bulma,dracula,monokai_pro,one_dark_pro';
-	static defaultTheme: string = import.meta.env.EYEGWAY_DEFAULT_THEME || 'bulma';
-	static layouts: string = import.meta.env.EYEGWAY_LAYOUTS || 'empty,2x2'
-	static defaultLayout: string = import.meta.env.EYEGWAY_DEFAULT_LAYOUT || 'empty';
+	static host: string = env.PUBLIC_EYEGWAY_HOST || 'http://localhost:55221';
+	static hubTriggerKey: string = env.PUBLIC_EYEGWAY_HUB_TRIGGER_KEY || '';
+	static title: string = env.PUBLIC_EYEGWAY_TITLE || 'eyegway';
+	static themes: string = env.PUBLIC_EYEGWAY_THEMES || 'ayu_light,ayu_mirage,bulma,dracula,monokai_pro,one_dark_pro';
+	static defaultTheme: string = env.PUBLIC_EYEGWAY_DEFAULT_THEME || 'bulma';
+	static layouts: string = env.PUBLIC_EYEGWAY_LAYOUTS || 'empty,2x2'
+	static defaultLayout: string = env.PUBLIC_EYEGWAY_DEFAULT_LAYOUT || 'empty';
 }
