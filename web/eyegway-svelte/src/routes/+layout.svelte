256123481287bdf05b5bd9f97ccfa74e7929c852
mmarcoo <marco.toschi@fastwebnet.it>
2024-12-17 17:00:01 +0100

    style: :bug: add proper title to the page

---
+++ web/eyegway-svelte/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Parameters } from '$lib/Parameters.js';
+	const title = Parameters.title;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+<slot />
