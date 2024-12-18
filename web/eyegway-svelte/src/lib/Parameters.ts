11a42f28f2a8ae1a758284e02adcbfb682b25944
mmarcoo <marco.toschi@fastwebnet.it>
2024-12-18 16:26:19 +0100

    fix: :pencil2: fix mini-typo in env defaults

--- web/eyegway-svelte/src/lib/Parameters.ts
+++ web/eyegway-svelte/src/lib/Parameters.ts
@@ -6,6 +6,6 @@ export class Parameters {
 	static title: string = env.PUBLIC_EYEGWAY_TITLE || 'eyegway';
 	static themes: string = env.PUBLIC_EYEGWAY_THEMES || 'ayu_light:ayu_mirage:bulma:dracula:monokai_pro:one_dark_pro';
 	static defaultTheme: string = env.PUBLIC_EYEGWAY_DEFAULT_THEME || 'bulma';
-	static layouts: string = env.PUBLIC_EYEGWAY_LAYOUTS || 'empty,2x2'
+	static layouts: string = env.PUBLIC_EYEGWAY_LAYOUTS || 'empty;2x2'
 	static defaultLayout: string = env.PUBLIC_EYEGWAY_DEFAULT_LAYOUT || 'empty';
 }
